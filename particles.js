99e1ac9f7d154a8ff60a3058369a78504611bac1
Neermay10 <74848380+Neermay10@users.noreply.github.com>
2020-12-24 17:05:25 +0530

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,23 @@
+class Particles{
+    constructor(x,y,r){
+        var opt={
+            friction:0,
+            restitution:0.5,
+            density:1
+        }
+        this.body=Bodies.circle(x,y,r/2,opt);
+        this.r=r;
+        this.color = color(random(0,255),random(0,255),random(0,255))
+        World.add(world,this.body)
+    }
+    display(){
+        fill(this.color);
+        noStroke();
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        ellipseMode(CENTER);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+}
